f3fc2fdcf2555e8f5108a90f11dc897f9f089837
nafisfuad8010-max <nafisfuad8010@gmail.com>
2026-07-27 23:56:52 +0600

    Declared semester course helpers

--- modules/courseResult.h
+++ modules/courseResult.h
@@ -1,15 +1,18 @@
 #ifndef COURSE_RESULT_H
 #define COURSE_RESULT_H
 
- #include "course.h"
+#include "course.h"
 
- typedef struct CourseResult
- {
-	 Course * course;
-	 double marks;
-	 } CourseResult;
+typedef struct CourseResult
+{
+    Course* course;
+    double marks;
+} CourseResult;
 
- CourseResult createCourseResult(Course * course, double marks);
- void viewCourseResult(CourseResult result);
+CourseResult createCourseResult(Course* course, double marks);
+void sortCourseResultsBySemester(CourseResult results[], int n_results);
+void filterCourseResultsBySemester(CourseResult results[], int n_results, int semester, CourseResult filtered[]);
+int countCourseResultsBeforeNull(CourseResult results[], int n_results);
+void viewCourseResult(CourseResult result);
 
- #endif
+#endif
